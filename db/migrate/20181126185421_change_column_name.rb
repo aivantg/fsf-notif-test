9cb9c8428dd8efb2790905fe53cc6dbc1a11ea72
aivantg <aivant.developer@gmail.com>
2018-11-26 10:55:13 -0800

    fixed column name

---
+++ db/migrate/20181126185421_change_column_name.rb
@@ -0,0 +1,5 @@
+class ChangeColumnName < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :devices, :type, :device_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_183531) do
+ActiveRecord::Schema.define(version: 2018_11_26_185421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_183531) do
     t.string "name"
     t.integer "version"
     t.string "uuid"
-    t.string "type"
+    t.string "device_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
